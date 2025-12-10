2a904abef56e6462489aec70762697fc87dfc241
QuanMou <2312038309@qq.com>
2025-12-10 15:56:58 +0800

    API式配置热点规则

--- sentinel-authorization-007/src/main/java/com/quan/sentinel/SentinelAuthorizationApplication.java
+++ sentinel-authorization-007/src/main/java/com/quan/sentinel/SentinelAuthorizationApplication.java
@@ -7,6 +7,8 @@ import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
 import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRuleManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -23,6 +25,18 @@ public class SentinelAuthorizationApplication {
 
 
         initWhiteList(); // API式配置白名单
+        initHotRule(); // API式配置热点规则
+    }
+
+    private static void initHotRule() {
+        List<ParamFlowRule> paramFlowRules = new ArrayList<>();
+        ParamFlowRule paramFlowRule = new ParamFlowRule();
+        paramFlowRule.setResource("test"); // 资源名
+        paramFlowRule.setParamIdx(0); // 参数索引
+        paramFlowRule.setGrade(RuleConstant.FLOW_GRADE_QPS); // QPS模式
+        paramFlowRule.setDurationInSec(10); // 统计窗口时长
+        paramFlowRules.add(paramFlowRule);
+        ParamFlowRuleManager.loadRules(paramFlowRules);
 
     }
 
@@ -34,6 +48,7 @@ public class SentinelAuthorizationApplication {
         authorityRule.setResource("list");
         rules.add(authorityRule);
         AuthorityRuleManager.loadRules(rules);
+
     }
 
 
